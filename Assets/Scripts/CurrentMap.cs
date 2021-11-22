78e52e5e51cac19a95d9a79bbff089a6d2bb11ae
lykelly19 <lykelly2014@icloud.com>
2021-11-21 19:42:33 -0500

    setup code for CurrentMap

---
+++ Assets/Scripts/CurrentMap.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrentMap : MonoBehaviour
+{
+    // string regionNameBank[];
+    int seconds = 0;
+
+    public int Seconds 
+    {
+        get { return seconds; }
+        set { seconds = value; }
+    }
+
+    // private bool guessRegion(string name, Point position) {}
+    // private Score changeScore(bool guessCorrect) {}
+    // private Score timerExpired() {}
+
+}
