b13a34ca5e088dba7378799e3838c2337af27c5d
lykelly19 <lykelly2014@icloud.com>
2021-12-03 16:20:46 -0500

    location match works with code

--- Assets/Scripts/BoxList.cs
+++ Assets/Scripts/BoxList.cs
@@ -31,7 +31,7 @@ public class BoxList
     public void populateList()
     {
         // each box has a public id
-        Box[] boxes = Object.FindObjectsOfType<Box>();
+        boxes = Object.FindObjectsOfType<Box>();
 
         // retrieve the location of each box
         foreach (Box b in boxes)
@@ -42,56 +42,54 @@ public class BoxList
         }
     }
 
-    // // Function is called when a match happens.
-    // // If so, this will update the box's label so
-    // // it will display the full name of the region.
-    // public void updateBoxLabel(string name)
-    // {
-    //     for(int i = 0; i < boxes.Length; i++)
-    //     {
-    //         if(boxes[i].name == name)
-    //         {
-    //             boxes[i].Label = boxes[i].name;
-    //             break;
-    //         }
-    //     }
-    // }
+    // Function is called when a match happens.
+    // If so, this will update the box's label so
+    // it will display the full name of the region.
+    public void updateBoxLabel(string name)
+    {
+        for(int i = 0; i < boxes.Length; i++)
+        {
+            if(boxes[i].name == name)
+            {
+                boxes[i].Label = boxes[i].name;
+                break;
+            }
+        }
+    }
 
     // /*
     //  * Searches list for a Box with a matching location
     //  * If found: return index
     //  * If DNE: return -1
     // */
-    // private int findLocationMatch(float posX, float posY)
-    // {
-    //     // for(int i = 0; i < boxes.Length; i++)
-    //     // {
-    //     //     float xDif = System.Math.Abs(posX - boxes[i].X);
-    //     //     float yDif = System.Math.Abs(posY - boxes[i].Y);
+    private int findLocationMatch(float posX, float posY)
+    {
+        for(int i = 0; i < boxes.Length; i++)
+        {
+            float xDif = System.Math.Abs(posX - boxes[i].X);
+            float yDif = System.Math.Abs(posY - boxes[i].Y);
 
-    //     //     // Check if within certain radius
-    //     //     // FIXME: pick a radius that makes sense. 0.1 was just a guess.
-    //     //     if(xDif < 0.1 && yDif < 0.1)
-    //     //     {
-    //     //         return i;
-    //     //     }
-    //     // }
+            // Check if within certain radius
+            // FIXME: pick a radius that makes sense. 0.1 was just a guess.
+            if(xDif < 0.1 && yDif < 0.1)
+            {
+                return i;
+            }
+        }
 
-    //     // return -1;
-    // }
+        return -1;
+    }
 
     public string findBoxMatch(Vector2 position)
     {
+        int index = findLocationMatch(position.x, position.y);
+
+        if(index >= 0 && index < boxes.Length)
+        {
+            return boxes[index].name;
+        }
 
         return null;
-        // int index = findLocationMatch(position.x, position.y);
-
-        // if(index >= 0 && index < boxes.Length)
-        // {
-        //     return boxes[index].name;
-        // }
-
-        // return null;
     }
 
     // // Resets Boxlist back to its original state.
