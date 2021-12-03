b76d7c3761a273f707581376a8678b4193c858e0
lykelly19 <lykelly2014@icloud.com>
2021-12-02 22:33:53 -0500

    work on removing all errors when starting game

--- Assets/Scripts/DisplayScore.cs
+++ Assets/Scripts/DisplayScore.cs
@@ -7,16 +7,16 @@ public class DisplayScore : MonoBehaviour
 
 {
 
-public Text scoreText;
+// public Text scoreText;
     // Start is called before the first frame update
     void Start()
     {
-     Map tempMap = GameManager.currentMap;
-     Score tempScore = tempMap.score;
-     int originalScore = tempScore.originalScore;
-     int levelMultiplier = tempScore.levelMultiplier;
-     int timeBonus = tempScore.timeBonus;
-     int finalScore = tempScore.currentScore;
-     scoreText.text = "Your Score: " + originalScore + " x " + levelMultiplier + " + " + timeBonus + " = " + finalScore;   
+    //  Map tempMap = GameManager.currentMap;
+    //  Score tempScore = tempMap.score;
+    //  int originalScore = tempScore.originalScore;
+    //  int levelMultiplier = tempScore.levelMultiplier;
+    //  int timeBonus = tempScore.timeBonus;
+    //  int finalScore = tempScore.currentScore;
+    //  scoreText.text = "Your Score: " + originalScore + " x " + levelMultiplier + " + " + timeBonus + " = " + finalScore;   
     }
 }
